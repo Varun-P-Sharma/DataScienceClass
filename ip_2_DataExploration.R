2665d55596bcb87ea6caa2f9c8c44e6936d62121
Wynne Moss <wynne.moss@colorado.edu>
2018-10-29 11:41:58 -0600

    add EDA and data

---
+++ ip_2_DataExploration.R
@@ -0,0 +1,15 @@
+#' ---
+#' output: github_document
+#' ---
+
+# Data Analysis # 
+
+## Data description==========
+# Data are parasite community data, analyzed over time.
+# Experimental design: visit 10 sites (ponds) 5 times each over the summer (2017). At each visit, # collect 10 individuals of each species and dissect.
+# For this analysis, I will model the abundance of one parasite species (Echinostoma). Response variable is number of parasites within an individual (Poisson distributed). Explanatory variable is visit date (fixed), site (random), individual relative size (fixed), and species (fixed).
+
+## Exploratory analyses========
+
+## Statistical analysis========
+## Inference========
