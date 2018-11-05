958d9ddfe78f5802a2cb1475c1cebc4584016212
Wynne Moss <wynne.moss@colorado.edu>
2018-11-05 12:52:46 -0700

    Added ants glmer with random effect for site

---
+++ W11_ants_lmer.R
@@ -0,0 +1,25 @@
+# ants dataset
+library(lme4)
+ants <- read.csv("data/ants.csv")
+head(ants)
+
+# Model equations
+# Richness ~ Habitat * Latitude + Elevation + (1|Site)
+# Elevation is a site-level covariate
+# Latitude is a site-level covariate
+# Richness is the response variable
+# Habitat is a observation-level covariate
+
+# Site is a grouping level
+# Habitat is not a grouping level since we want to treat it as a fixed effect
+
+ant.lmer <- glmer(richness ~ habitat*scale(latitude) + scale(elevation) + (1|site)-1, data = ants, family = poisson)
+summary(ant.lmer)
+
+# Intercept is the log(richness) at the average latitude for bog sites at sea level
+# Habitat forest is the increase in richness for forests at sea level and mean latitude
+# For BOG sites, each SD increase in latitude results in a 0.23 decline in log(richness)
+# For all sites, each SD increase in elevation lowers log(richness) by -0.19
+# In forests, the slope of latitude on richness is steeper, but not significantly so
+
+
