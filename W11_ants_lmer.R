9e91805f9cbf6d284d86710ab05f76f5be4e12f8
Wynne Moss <wynne.moss@colorado.edu>
2018-11-05 13:43:55 -0700

    Added model formula

--- W11_ants_lmer.R
+++ W11_ants_lmer.R
@@ -13,8 +13,18 @@ head(ants)
 # Site is a grouping level
 # Habitat is not a grouping level since we want to treat it as a fixed effect
 
+# Richness[i] ~ Poisson(mu[i])
+
+# log(mu[i]) ~ alpha[i,j] + beta_1*latitude + beta_2*habitat + beta_12 (latitude*habitat) + beta_4*elevation
+
+# alpha[j] ~ normal(mu_alpha, sigma2_alpha)
+
+
+
 ant.lmer <- glmer(richness ~ habitat*scale(latitude) + scale(elevation) + (1|site)-1, data = ants, family = poisson)
 summary(ant.lmer)
+
+
 
 # Intercept is the log(richness) at the average latitude for bog sites at sea level
 # Habitat forest is the increase in richness for forests at sea level and mean latitude
