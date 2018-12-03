87d3864907bd4c4fc553289e72ce5882d23311e5
Wynne Moss <wynne.moss@colorado.edu>
2018-12-03 15:22:38 -0700

    I still need to work on this...adding names to vectors will help keep this straight

--- ip_4_simulations.R
+++ ip_4_simulations.R
@@ -4,7 +4,7 @@ nsites <- 10
 site.means.gamma <- NA
 beta.t.sites <- NA
 visits <- 1:5
-
+nvisits <- 5
 parasite.sim.data <- expand.grid("Sites" = LETTERS[1:10], "Visit" = 1:5,
                                  "Species" = c("PSRE", "TATO"), "SVL" = seq(-1,1,.2))
 parasite.sim.data$collection <- paste(parasite.sim.data$Sites, parasite.sim.data$Visit)
@@ -18,13 +18,23 @@ site.means.gamma <- rnorm(n = nsites, mean = 0.5, sd = .2)
 beta.t.sites <- rnorm(n = nsites, mean = 0.2, sd = .05)
 
 # Calculate a predicted parasite load at time point t for a given site (a sample mean)
-# Integrate this into the data-frame?
-# loop through the sites and do this?
+# I'm going to loop through the sites and do this, because I don't know how else to do it.
+site.visits.exp <- list()
 
-exp.samp.mean <- site.means.gamma + beta.t.sites*visits
+for(i in 1:nsites){
+  site.visits.exp[[i]] <- site.means.gamma[i] + beta.t.sites[i]*visits
+}
 
-# Simulate the sample visit mean randomly from that predicted value
+# OK now we have 50 samples and their means calculated (5 from each of 10 sites)
 
+# Simulate the sample visit mean randomly from that predicted value (this will be 50 different alphas)
+# collapse all the site visits.exp (mu's) into a vector
+
+site.visits.exp <- unlist(site.visits.exp)
+alpha.sites <- rnorm(n = nsites*nvisits, mean = site.visits.exp, sd = .2)
 
 # Now, simulate how much each individual varies from that sample visit based on species and snout vent length
+
+# each individual's covariates are in the dataframe
+head(parasite.sim.data)
 
